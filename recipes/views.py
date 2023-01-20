18f534621647d3f2461929aace16a9c0bb028db5
Jon Souza <jhonatah197@gmail.com>
2023-01-19 21:35:52 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,12 +7,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'nome': 'Jon Souza',
     })
-
-def contato(request):
-    return HttpResponse('contato')
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-def _home(request):
-    return HttpResponse('HOME')
